b94bb7cba3f56144cb3ba2c838e75160dbec4384
saravindswamy <75692841+saravindswamy@users.noreply.github.com>
2021-02-06 22:19:26 +0530

    initial commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function About() {
+  return (
+    <div className="App">
+      <h1>About</h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Shop.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function Shop() {
+  return (
+    <div className="App">
+      <h1>Shop</h1>
+    </div>
+  );
+}
+
+export default Shop;
